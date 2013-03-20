146bd27b72067b85a1e0bfa990450817009272dc
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-03-20 14:57:42 -0700

    Password checker step 1

--- checker_spec.rb
+++ checker_spec.rb
@@ -3,21 +3,31 @@ require 'rspec'
 require_relative 'checker'
 
 describe "Checker" do
-  before(:each) do
-    @sut = Checker.new
-  end
-  it "should reject passwords that are less than 7 characters" do
-    @sut.check("abc").should == false
-  end
+  context "Regular User Passwords" do
+    before(:each) do
+      @sut = Checker.new
+    end
+    it "should reject passwords that are less than 7 characters" do
+      @sut.check("abc").should == false
+    end
 
-  it "should reject passwords that dont have atleast 1 alphanumeric characters" do
-    @sut.check("1277777777773").should == false
-  end
+    it "should reject passwords that dont have atleast 1 alphanumeric characters" do
+      @sut.check("1277777777773").should == false
+    end
 
-  it "should reject passwords that don't have at least 1 digit" do
-    @sut.check("PASSWORD").should == false
+    it "should reject passwords that don't have at least 1 digit" do
+      @sut.check("PASSWORD").should == false
+    end
+    it "should not reject passwords that use 0 as the digit" do
+      @sut.check("PASSWORD0").should == true
+    end
   end
-  it "should not reject passwords that use 0 as the digit" do
-    @sut.check("PASSWORD0").should == true
+  context "Admin User Passwords" do
+    before(:each) do
+      @sut = Checker.new
+    end
+    it " Password should be more than 10 characters" do
+       @sut.check("12777777").should == false
+    end
   end
 end
