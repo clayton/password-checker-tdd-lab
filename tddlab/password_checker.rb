e06c39aea76e82f5f7dd5cea2bd68ecd57fa1fad
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-07-29 10:45:19 -0700

    Refactor

--- tddlab/password_checker.rb
+++ tddlab/password_checker.rb
@@ -1,13 +1,19 @@
 class PasswordChecker
   def check(password)
     return false unless has_a_digit?(password)
-    return false unless password.match(/[a-zA-Z]/)
-    return false if password.length < 7
+    return false unless has_a_letter?(password)
+    return false if long_enough?
     true
   end
 
 private
   def has_a_digit?(password)
     password.match(/[0-9]/)
+  end
+  def has_a_letter?(password)
+    password.match(/[a-zA-Z]/)
+  end
+  def long_enough?(password)
+    password.length < 7
   end
 end
